cfcf028f6f523a9c5661bbd7d72139529300635d
tarun6078 <88580541+tarun6078@users.noreply.github.com>
2022-10-02 17:28:05 +0530

    3rd year

--- ece/lectures.js
+++ ece/lectures.js
@@ -1,4 +1,4 @@
-setTitle('ECE 2nd Year')
+setTitle('ECE 3rd Year')
 
 addTime('9:00 to 10:00');
 addTime('10:00 to 11:00');
@@ -11,36 +11,29 @@ addTime('16:00 to 17:00');
 addTime('17:00 to 18:00');
 
 
-addLecture('Engineering Mechanics (Lecture)', 3, 4, 1);
-addLecture('Engineering Mechanics (Lecture)', 3, 5, 1);
-addLecture('Engineering Mechanics (Lecture)', 3, 6, 1);
-addLecture('Engineering Mechanics (Tutorial) EC1', 4, 6, 1);
-addLecture('Engineering Mechanics (Tutorial) EC2', 6, 3, 1);
-addLecture('Analog Communication (Lecture)', 2, 4, 1);
-addLecture('Analog Communication (Lecture)', 2, 1, 1);
-addLecture('Analog Communication (Lecture)', 2, 6, 1);
-addLecture('Analog Communication (Tutorial) EC2', 4, 3,1);
-addLecture('Analog Communication (Tutorial) EC1', 6, 2, 1);
-addLecture('Analog Communication (Tutorial) EC3', 7, 5, 1);
-addLecture('Analog Circuits (Lecture)', 2, 2, 1);
-addLecture('Analog Circuits (Lecture)', 2, 3, 1);
-addLecture('Analog Circuits (Lecture)', 1, 4, 1);
-addLecture('Engineering Mechanics (Tutorial) EC3(15:00-16:00) , Analog Circuits Lab EC1', 7, 1, 2);
-addLecture('Analog Circuits Lab EC2 , Microcontrollers Lab EC3', 7, 2, 2);
-addLecture('Analog Circuits Lab EC3 , Microcontrollers Lab EC1 ', 7, 3, 2);
-addLecture('Microcontrollers (Lecture)', 1, 1, 1);
-addLecture('Microcontrollers (Lecture)', 1, 2, 1);
-addLecture('Antennas & Propagation (Lecture)', 3, 1, 1);
-addLecture('Microcontrollers Lab EC2', 7, 4, 2);
-addLecture('Antennas & Propagation (Lecture)', 3, 2, 1);
-addLecture('Antennas & Propagation (Lecture)',3, 3, 1);
-addLecture('Antennas & Propagation (Tutorial) EC1', 4, 1, 1);
-addLecture('Antennas & Propagation (Tutorial) EC2', 4, 2, 1);
-addLecture('Antennas & Propagation (Tutorial) EC3', 6, 5, 1);
-addLecture('Probability & Statistics (Lecture)', 1, 3, 1);
-addLecture('Probability & Statistics (Lecture)', 1, 5, 1);
-addLecture('Probability & Statistics (Lecture)', 1, 6, 1);
-addLecture('Probability & Statistics (Tutorial) EC1 ', 4, 4, 1);
-addLecture('Probability & Statistics (Tutorial) EC2', 4, 5, 1);
-addLecture('Probability & Statistics (Tutorial) EC3', 6 , 4, 1);
-addLecture('NSS EC1, EC2, EC3', 6, 6, 4);
+addLecture('Microwave Theory & Techniques (Lecture)', 2, 1, 1);
+addLecture('Microwave Theory & Techniques (Lecture)', 2, 3, 1);
+addLecture('Microwave Theory & Techniques (Lecture)', 3, 5, 1);
+addLecture('Microwave Theory& Techniques EC1 , Communication EC2 & Computer Lab EC3 (Lab)', 7, 5, 2);
+addLecture('Microwave Theory & Techniques (Lab) EC2', 7, 4, 2);
+addLecture('Microwave Theory & Techniques EC3 , Communication EC1 & Computer Lab EC2 (Lab)', 7, 3, 2);
+addLecture('Digital Communication (Lecture)', 1, 1, 1);
+addLecture('Digital Communication (Lecture)', 1, 3, 1);
+addLecture('Digital Communication (Lecture)', 1, 5,1);
+addLecture('Digital Communication (Tutorial) EC2 4-5 , Compute  Lab EC1 & Comm. Lab EC3 3-5  ',7 , 1, 2);
+addLecture('Digital Comm. EC3 & DSP EC1 (Tutorial)', 8, 2, 1);
+addLecture('Digital Communication EC1 (Tutorial) ', 6, 3, 1);
+addLecture('DSP (Lecture)', 1, 2, 1);
+addLecture('DSP (Lecture)', 1, 4, 1);
+addLecture('DSP (Lecture)',1, 6, 1);
+addLecture('DSP (Tutorial) EC2', 7, 2, 1);
+addLecture('DSP (Tutorial) EC3', 2, 6, 1);
+addLecture('Neural Processing & System (Lecture)', 2, 2, 1);
+addLecture('Neural Processing & System (Lecture)', 3, 3, 1);
+addLecture('Neural Processing & System (Lecture)', 2, 5, 1);
+addLecture('COA (Lecture) ', 3, 1, 1);
+addLecture('COA (Lecture)', 2, 4, 1);
+addLecture('NSS EC1, EC2, EC3', 6, 6, 3);
+addLecture('Open Elective (Lecture)', 4, 1, 1);
+addLecture('Open Elective (Lecture)', 4, 2, 1);
+addLecture('Open Elective (Lecture)', 4, 3, 1);
